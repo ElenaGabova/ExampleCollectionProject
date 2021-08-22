1753bb1bd71d47e2792c8622ee2ce0cb2d5d4926
Елена Белых <Eleyurgab@gmail.com>
2021-08-22 14:25:14 +0300

    Добавьте файлы проекта.

---
+++ ExampleCollectionClass/CollectionClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleCollectionClass
+{
+    /* Обобщенный класс, который может хранить объекты любого типа.
+     * В классе реализованы:
+     * 1. метод для добавления данных в список
+     * 2. метод удаления из список
+     * 3. метод получения элемента из списка по индексу 
+     * 4. метод, возвращающий длину списка.
+     * */
+    class CollectionClass<Type>
+    {
+        public List<Type> Collection { get; set; } = new List<Type>();
+
+        public int Count()
+        {
+            return Collection.Count;
+        }
+
+        public CollectionClass(params Type[] elements)
+        {
+            foreach (var element in elements)
+                Collection.Add(element);
+        }
+
+        public void AddElement(Type element)
+        {
+            Collection.Add(element);
+        }
+
+        public void DeleteElement(Type element)
+        {
+            Collection.Remove(element);
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            foreach (var element in Collection) { 
+                    result += element.ToString() + ", ";
+            }
+            return result;
+        }
+
+        public Type this[int index]
+        {
+            get
+            {
+                return Collection[index];
+            }
+            set
+            {
+                Collection[index] = value;
+            }
+        }
+    }
+}
---
+++ ExampleCollectionClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleCollectionClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CollectionClass<int> intCollection = new CollectionClass<int> {
+                                                                                Collection = new List<int> { 1, 2, 3, 10 }
+                                                                          };
+
+            intCollection.AddElement(4);
+            intCollection.DeleteElement(1);
+
+            Console.WriteLine("Количество элементов обобщенного массива: " + intCollection.Count().ToString());
+            Console.WriteLine("Элементы обобщенного массива: " + intCollection.ToString());
+
+
+            CollectionClass<string> stringCollection = new CollectionClass<string>
+            {
+                Collection = new List<string> { "1", "2", "3", "10", "4 элемент" }
+            };
+
+            Console.WriteLine();
+            Console.WriteLine("Количество элементов обобщенного массива: " + stringCollection.Count().ToString());
+            Console.WriteLine("Элементы обобщенного массива: " + stringCollection.ToString());
+
+            stringCollection.Collection[0] = "1 элемент";
+            Console.WriteLine("Элементы обобщенного массива после замены 1 элмента: " + stringCollection.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
